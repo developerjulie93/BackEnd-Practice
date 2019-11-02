ca98a7471d67571f2d884fcd0fcf4311b6c9acfb
developerjulie93 <56218979+developerjulie93@users.noreply.github.com>
2019-11-01 19:34:49 -0700

    Add files via upload

---
+++ temp/index.php
@@ -0,0 +1,56 @@
+<?php
+  $conn = mysqli_connect('localhost','root','111111');
+  mysqli_select_db($conn, 'opentutorials');
+  $result = mysqli_query($conn, "SELECT * FROM topic");
+
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8">
+     <style type="text/css">
+       body.white{
+        background: white;
+        color: black;
+       }
+       body.black{
+        background-color: black;
+        color: white;
+       }
+     </style>
+</head>
+<body id="target">
+    <header>
+        <h1><a href="http://localhost/index.php">JavaScript</a></h1>
+  </header>
+    <nav>
+        <ol style="list-style: none; padding:0;">
+          <?php
+              while($row = mysqli_fetch_assoc($result)){
+                echo '<li><a href="http://localhost/temp/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>';
+             }
+           ?>
+        </ol>
+    </nav>
+  <div id="control">
+    <input type="button" value="white" onclick="document.getElementById('target').className='white'"/>
+    <input type="button" value="black" onclick="document.getElementById('target').className='black'" />
+    <a href="http://localhost/temp/write.php"><input type="button" value="write"></a>
+  </div>
+  <article style="width: 500px; height: 500px; background-color: yellow;">
+  <?php
+    if(empty($_GET['id']) == false ) {
+      $sql = 'SELECT topic.id,title,description,name  FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id='.$_GET['id'];
+      $result = mysqli_query($conn, $sql);
+      $row = mysqli_fetch_assoc($result);
+      echo '<h2>'.$row['title'].'</h2>';
+      echo '<h3>'.$row['name'].'</h3>';
+      echo '<p>'.$row['description'].'</p>';
+  }
+     ?>
+
+   </article>
+
+</body>
+</html>
